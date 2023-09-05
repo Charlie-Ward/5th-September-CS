2fbc0d1cf0c406ffa7dd265613d3f7be612345a8
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-09-05 09:59:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 myList = [1, 4, 6, 9, 10, 56, 42, 36, 90, 21, 65, 32, 67, 85, 1, 56, 753, 133, 632, 123, 789, 86, 432, 56, 43, 212, 789,
           6313, 3467, 43, 224, 6432, 5774, 21, 3, 68, 0, 6, 42, 1, 2, 4, 57]
-print(len(myList))
-num = int(input("Type the number to search for"))
+found = False
+
+num = int(input("Type the number to search for "))
+
+while found is False:
+    if myList[i] == num:
+        
